85f0025b4cdec9b79fc0cdf9ae8a0e5d61d23406
Andres Sosa <ASosa@SecuritysSciences.com>
2014-02-28 17:06:26 -0700

    Working

--- specs/MsAccountSetupSrv/equipmentLookup.spec.js
+++ specs/MsAccountSetupSrv/equipmentLookup.spec.js
@@ -22,6 +22,10 @@ describe("EquipmentLookup.spec tests. | ", function() {
   });
   // **   END BEFORE EACH.
 
+  /** Initialize. */
+  var equipment1 = 'GEC-TX1510011', // RECESSED DOOR/WINDOW 
+    equipment2 = 'GEC-60362N103195'; // DOOR/WINDOW 
+
   /** Lookup by Part Number. */
   it("Validate getting EquipmentLookup by PartNumber", function(done) {
     dataSource.createMsAccountScratch(request, {
@@ -54,7 +58,54 @@ describe("EquipmentLookup.spec tests. | ", function() {
 
     }, function(accountValue) {
       request.get({
-          url: config.SseServicesCmsCORS + "MsAccountSetupSrv/Equipments/GEC-TX1510011/ByPartNumber?accountId=" + accountValue.AccountID,
+          url: config.SseServicesCmsCORS + "MsAccountSetupSrv/Equipments/" + equipment1 + "/ByPartNumber?id=" + accountValue.AccountID,
+          json: true
+        },
+        function(error, response, body) {
+          console.log("Body: ", body);
+          expect(error).toBeNull();
+          expect(body).not.toBeNull();
+          expect(body.Code).toBe(0, "Error on call.  Message: " + body.Message);
+
+          modelDef.zoneEventType(body.Value);
+          done();
+        });
+    });
+  });
+
+  /** Lookup by Barcode. */
+  it("Validate getting EquipmentLookup by barcode", function(done) {
+    dataSource.createMsAccountScratch(request, {
+      DealerId: 5001,
+      SeasonId: 1,
+      TeamLocationId: 1,
+      SalesRepId: 'SOSA001',
+      StreetAddress: '1128 E 3300 S',
+      City: 'Salt Lake City',
+      StateId: 'UT',
+      PostalCode: '84106',
+      PhoneNumber: '(801) 486-8251',
+      LocalizationId: 'en-us', // *REQUIRED:  This is the default language that they speak.
+      LeadSourceId: '7', // *REQUIRED:  This is the web.
+      LeadDispositionId: 9, // *REQUIRED:  This is in what state is the lead.  9: Means "Ran Credit"
+      Salutation: 'Mr.', // *OPTIONAL
+      FirstName: 'Kirby', // *REQUIRED
+      MiddleName: null, // *OPTIONAL
+      LastName: 'Vacuum', // *REQUIRED
+      Suffix: 'II', // *OPTIONAL
+      Gender: 'Male', // *REQUIORED
+      SSN: '333-33-3333', // *CONDITIONAL:  Either SSN or DOB has to be present or both.
+      DOB: '12/14/1968', // *CONDITIONAL:  Either SSN or DOB has to be present or both.
+      Dl: '2342423423', // *OPTIONAL
+      DlStateId: 'UT', // *OPTIONAL
+      Email: 'sammy@sam.com', // *REQUIORED
+      PhoneHome: '(801) 654-9877', // *OPTIONAL
+      PhoneWork: null, // *OPTIONAL
+      PhoneMobile: '(909) 987-6790' // *OPTIONAL
+
+    }, function(accountValue) {
+      request.get({
+          url: config.SseServicesCmsCORS + "MsAccountSetupSrv/Equipments/" + equipment2 + "/ByBarcode?accountId=" + accountValue.AccountID,
           json: true
         },
         function(error, response, body) {
@@ -67,26 +118,6 @@ describe("EquipmentLookup.spec tests. | ", function() {
           done();
 
         });
-
     });
   });
-
-  /** Lookup by Barcode. */
-  it("Validate getting EquipmentLookup by barcode", function(done) {
-    request.get({
-        url: config.SseServicesCmsCORS + "MsAccountSetupSrv/Equipments/23423sdsf23/ByBarcode?accountId=1000",
-        json: true
-      },
-      function(error, response, body) {
-        console.log("Body: ", body);
-        expect(error).toBeNull();
-        expect(body).not.toBeNull();
-        expect(body.Code).toBe(0, "Error on call.  Message: " + body.Message);
-
-        modelDef.zoneEventType(body.Value);
-        done();
-
-      });
-  });
-
 });
